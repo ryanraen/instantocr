d7a9e607dc5ddf01b0f449602bca4ace8eb117c3
Ryan Xie <yxie84@students.cs.ubc.ca>
2025-01-27 18:41:27 -0800

    Specify ImageToTextApp class

--- src/main/ui/ImageToTextApp.java
+++ src/main/ui/ImageToTextApp.java
@@ -1,6 +1,59 @@
 package ui;
 
-// Image to text conversion application
+// Image to text conversion application with 
+// a conversion instance and conversion history
 public class ImageToTextApp {
+
+    // EFFECTS: runs the image to text application
+    public ImageToTextApp() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user input
+    private void processInput() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes conversion instance and conversion history
+    private void init() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: handles user commands
+    private void handleCommand(String command) {
+        // stub
+    }
+
+    // REQUIRES: path must be a valid file path to an image on the user device
+    // MODIFIES: this
+    // EFFECTS: reads an image file, stores it in an 
+    // ImageConversion instance, and saves it to the conversion history
+    private void uploadImage(String path) {
+        // stub
+    }
+
+    // EFFECTS: displays user command choices
+    private void displayCommands() {
+        // stub
+    }
+
+    // EFFECTS: displays the user's conversion history
+    // in the current session as a list, and allows user
+    // to select a previous conversion for review
+    private void displayHistory() {
+        // stub
+    }
+
+    // REQUIRES: id < size of conversion history, and
+    // id must be the index of a valid ImageConversion 
+    // instance in the conversion history
+    // EFFECTS: displays the file path and extracted text from
+    // a previous image conversion
+    private void displayPrevConversion(int id) {
+        // stub
+    }
     
 }
