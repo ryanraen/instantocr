09ef57909436229faed8a14f85b7702ade26801e
Ryan Xie <yxie84@students.cs.ubc.ca>
2025-02-06 17:33:15 -0800

    Added test methods in TestCharacterTemplate class

--- src/test/model/TestCharacterTemplate.java
+++ src/test/model/TestCharacterTemplate.java
@@ -1,5 +1,25 @@
 package model;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.opencv.imgcodecs.Imgcodecs;
+
 public class TestCharacterTemplate {
+
+    CharacterTemplate template;
+
+    @BeforeEach
+    public void setup() {
+        template = new CharacterTemplate("data/templates/a.png");
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals("data/templates/a.png", template.getFilePath());
+        assertEquals(Imgcodecs.imread("data/templates/a.png"), template.getImageMat());
+        assertEquals('a', template.getLabel());
+    }
     
 }
