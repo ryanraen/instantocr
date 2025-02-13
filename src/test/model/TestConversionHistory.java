11fd9bf120f78cece6993b7609fc9b88066b25e5
Ryan Xie <yxie84@students.cs.ubc.ca>
2025-02-13 15:26:07 -0800

    Added ConversionHistory tests to reflect added not found case in ConversionHistory

--- src/test/model/TestConversionHistory.java
+++ src/test/model/TestConversionHistory.java
@@ -1,9 +1,12 @@
 package model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TestConversionHistory {
     ConversionHistory history;
@@ -27,7 +30,7 @@ public class TestConversionHistory {
         assertEquals(0, history.getConversions().size());
         history.addConversion(conversion1);
         assertEquals(1, history.getConversions().size());
-        assertEquals(conversion1, history.getByIndex(0));
+        assertEquals(conversion1, history.getByIndex(1));
     }
 
     @Test
@@ -74,6 +77,14 @@ public class TestConversionHistory {
     }
 
     @Test
+    public void testGetByFilePathNotFound() {
+        assertEquals(0, history.getConversions().size());
+        history.addConversion(conversion1);
+        history.addConversion(conversion2);
+        assertNull(history.getByFilePath("data/templates/z.png"));
+    }
+
+    @Test
     public void testClearEmptyOnce() {
         assertEquals(0, history.getConversions().size());
         history.clear();
@@ -108,7 +119,7 @@ public class TestConversionHistory {
         assertEquals(0, history.getConversions().size());
         history.addConversion(conversion2);
         assertEquals(1, history.getConversions().size());
-        history.deleteByFilePath("data/templates/a.png");
+        assertTrue(history.deleteByFilePath("data/templates/a.png"));
         assertEquals(0, history.getConversions().size());
     }
 
@@ -118,9 +129,20 @@ public class TestConversionHistory {
         history.addConversion(conversion2);
         history.addConversion(conversion1);
         assertEquals(2, history.getConversions().size());
-        history.deleteByFilePath("data/images/simple_sentence.png");
+        assertTrue(history.deleteByFilePath("data/images/simple_sentence.png"));
         assertEquals(1, history.getConversions().size());
         assertEquals(conversion2, history.getByIndex(1));
     }
     
+    @Test
+    public void testDeleteByFilePathNotFound() {
+        assertEquals(0, history.getConversions().size());
+        history.addConversion(conversion2);
+        history.addConversion(conversion1);
+        assertEquals(2, history.getConversions().size());
+        assertFalse(history.deleteByFilePath("data/images/simple_sentence.png"));
+        assertEquals(2, history.getConversions().size());
+        assertEquals(conversion1, history.getByIndex(1));
+        assertEquals(conversion2, history.getByIndex(2));
+    }
 }
