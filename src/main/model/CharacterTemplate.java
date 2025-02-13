21f5a57b142ccadf30a003162998086de542b677
Ryan Xie <yxie84@students.cs.ubc.ca>
2025-02-13 14:44:40 -0800

    Revised CharacterTemplate specification to store identity as String instead of char

--- src/main/model/CharacterTemplate.java
+++ src/main/model/CharacterTemplate.java
@@ -1,16 +1,18 @@
 package model;
 
 import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 
 // Represents a character template, having a file path, 
 // a matrix that holds all pixels in the template image, 
-// and a character holding the English alphabet identity of the template
+// and a String holding the English alphabet identity of the template
 public class CharacterTemplate {
 
     // REQUIRES: path be a relative path pointing to a valid template
     // image in the project data
     // EFFECTS: initializes the character template and reads
-    // the image file at path
+    // the image file at path, storing its file name as 
+    // the label (excluding file extension)
     public CharacterTemplate(String path) {
         // stub
     }
