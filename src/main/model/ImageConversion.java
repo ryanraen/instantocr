5e973dcd348af940a05d758d978ec957ccd7eb81
Ryan Xie <yxie84@students.cs.ubc.ca>
2025-02-14 02:35:54 -0800

    Added implementation for ImageConversion

--- src/main/model/ImageConversion.java
+++ src/main/model/ImageConversion.java
@@ -1,8 +1,16 @@
 package model;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 // Represents an image conversion having a file path, matrix that holds the image
 // a list of matrices holding sliced sub-images of individual characters, 
@@ -10,48 +18,74 @@ import org.opencv.core.Mat;
 // the standard width and height of comparison templates,
 // a list of character templates, and the extracted text
 public class ImageConversion {
+    public static final int POLARIZE_THRESHOLD = 101;
+    public static final int TEMPLATE_WIDTH = 8;
+    public static final int TEMPLATE_HEIGHT = 16;
+    private String filePath;
+    private Mat imageMat;
+    private List<Mat> subImages;
+    private List<CharacterTemplate> templates;
+    private String extractedText;
 
     // EFFECTS: initializes filePath, extractedText as empty Strings, and
-    // initializes imageMat, subImages, and templates as null
+    // initializes imageMat as null, subImages as empty list,
+    // and set up all templates in data/templates
     public ImageConversion() {
-        // stub
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        this.filePath = "";
+        this.imageMat = null;
+        this.subImages = new ArrayList<>();
+        this.templates = new ArrayList<>();
+        this.extractedText = "";
+
+        initTemplates();
     }
 
     // EFFECTS: initializes filePath with given filePath, and set
     // appropriate values for all other fields (refer to void constructor)
     public ImageConversion(String filePath) {
-        // stub
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        this.filePath = filePath;
+        this.imageMat = Imgcodecs.imread(filePath);
+        this.subImages = new ArrayList<>();
+        this.templates = new ArrayList<>();
+        this.extractedText = "";
+
+        initTemplates();
     }
 
     public String getFilePath() {
-        return null; // stub
+        return this.filePath;
     }
 
     public Mat getImageMat() {
-        return null; // stub
+        return this.imageMat;
     }
 
     public List<Mat> getSubImages() {
-        return null; // stub
+        return this.subImages;
     }
 
     public double getThreshold() {
-        return 0; // stub
+        return POLARIZE_THRESHOLD;
     }
 
     public List<CharacterTemplate> getTemplates() {
-        return null; // stub
+        return this.templates;
     }
 
     public String getExtractedText() {
-        return null; // stub
+        return this.extractedText;
     }
 
     // MODIFIES: this
     // EFFECTS: reads all template images in data/templates
     // and stores in character templates
     public void initTemplates() {
-        // stub
+        File[] files = new File("data/templates").listFiles();
+        for (File file : files) {
+            templates.add(new CharacterTemplate(file.getPath()));
+        }
     }
 
     // REQUIRES: this image conversion instance must have an existing
@@ -59,7 +93,7 @@ public class ImageConversion {
     // MODIFIES: this
     // EFFECTS: reads the image at filePath and stores it in imageMat
     public void readImage() {
-        // stub
+        this.imageMat = Imgcodecs.imread(filePath);
     }
 
     // REQURES: path must be a valid file path to an image in data/images
@@ -67,7 +101,8 @@ public class ImageConversion {
     // EFFECTS: stores path as filePath, reads the image at path,
     // and stores it in imageMat
     public void readImage(String path) {
-        // stub
+        this.filePath = path;
+        this.imageMat = Imgcodecs.imread(path);
     }
 
     // REQUIRES: imageMat must be a valid image containing lines
@@ -75,63 +110,153 @@ public class ImageConversion {
     // MODIFIES: this
     // EFFECTS: processes image and saves recognized text in extractedText
     public void processImage() {
-        // stub
+        toGrayscale();
+        polarizeImage(imageMat, POLARIZE_THRESHOLD);
+        sliceImage();
+        for (Mat image : subImages) {
+            resizeImage(image, TEMPLATE_WIDTH, TEMPLATE_HEIGHT);
+            this.extractedText += compareWithTemplates(image, templates);
+        }
     }
 
     // REQUIRES: imageMat must contain a standard RGB image
     // MODIFIES: this
     // EFFECTS: converts image to grayscale
     public void toGrayscale() {
-        // stub
+        Imgproc.cvtColor(imageMat, imageMat, Imgproc.COLOR_BGR2GRAY);
     }
 
     // MODIFIES: this
-    // EFFECTS: crops image to the outermost black pixel
-    public void crop(Mat image) {
-        // stub
+    // EFFECTS: crops image to the outermost colorRGB pixel
+    public Mat crop(Mat image, double colorRGB) {
+        int minY = image.rows();
+        int minX = image.cols();
+        int maxY = 0;
+        int maxX = 0;
+
+        for (int y = 0; y < image.rows(); y++) {
+            for (int x = 0; x < image.cols(); x++) {
+                if (image.get(y, x)[0] != colorRGB) {
+                    if (y < minY) {
+                        minY = y;
+                    } else if (y > maxY) {
+                        maxY = y;
+                    }
+                    if (x < minX) {
+                        minX = x;
+                    } else if (x > maxX) {
+                        maxX = x;
+                    }
+                }
+            }
+        }
+
+        return new Mat(image, new Rect(minX, minY, (maxX - minX) + 1, (maxY - minY) + 1));
     }
 
-    // REQUIRES: imageMat must contain an image in grayscale
+    // REQUIRES: image must contain an image in grayscale
     // MODIFIES: this
     // EFFECTS: convert all pixels to either black or white using numerical
     // threshold
-    public void polarizeImage() {
-        // stub
+    public void polarizeImage(Mat image, int threshold) {
+        Imgproc.adaptiveThreshold(image, image, 255,
+                Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C,
+                Imgproc.THRESH_BINARY,
+                threshold, 5);
     }
 
-    // REQUIRES: imageMat must contain an image polarized grayscale image
+    // REQUIRES: imageMat must contain a grayscale image
     // there must be at least 1 pixel of white margin between adjacent
     // text characters in the image
     // MODIFIES: this
     // EFFECTS: slices image into a list of sub-images containing
     // 1 text character each, in the order that they appear left-to-right
     public void sliceImage() {
-        // stub
-        // Note to self: slice out rows first then columns
+        List<Mat> rows = new ArrayList<>();
+        double whiteRGB = 255.0;
+
+        List<Integer> rowsPos = sliceHorizontal(imageMat, whiteRGB);
+        for (int i = 1; i < rowsPos.size() - 1; i += 2) {
+            int row = rowsPos.get(i);
+            int next = rowsPos.get(i + 1);
+            Mat subImage = new Mat(imageMat, new Rect(0, row, imageMat.cols(), next - row));
+            rows.add(subImage);
+        }
+
+        List<List<Integer>> colsPos = sliceVertical(rows, whiteRGB);
+        for (int i = 0; i < rows.size(); i++) {
+            for (int j = 0; j < colsPos.get(i).size() - 1; j += 2) {
+                int col = colsPos.get(i).get(j);
+                int next = colsPos.get(i).get(j + 1);
+                Mat subImage = new Mat(rows.get(i), new Rect(col, 0, (next - col), rows.get(i).rows()));
+                subImage = crop(subImage, whiteRGB);
+                subImages.add(subImage);
+            }
+        }
     }
 
-
-
-    
     // REQUIRES: image is a grayscale image matrix
     // EFFECTS: slices image horizontally to return
     // list of row indices bounding sub images
     public List<Integer> sliceHorizontal(Mat image, double whiteRGB) {
-        return null; // stub
+        List<Integer> rowsPos = new ArrayList<>();
+        Boolean onWhiteRow = false;
+
+        for (int row = 0; row < image.rows(); row++) {
+            Boolean allWhiteRow = true;
+            for (int col = 0; col < image.cols(); col++) {
+                if (image.get(row, col)[0] != whiteRGB) {
+                    allWhiteRow = false;
+                    break;
+                }
+            }
+            if (!onWhiteRow && allWhiteRow) {
+                onWhiteRow = true;
+                rowsPos.add(row);
+            } else if (onWhiteRow && !allWhiteRow) {
+                onWhiteRow = false;
+                rowsPos.add(row);
+            }
+        }
+        return rowsPos;
     }
 
     // REQUIRES: rows contains lines of text sliced by sliceHorizontal
-    // EFFECTS: slices image rows vertically and adds all single character
-    // sub images to subImages
-    public void sliceVertical(List<Mat> rows, double whiteRGB) {
-        // stub
+    // EFFECTS: slices image vertically to return
+    // list of list of col indices bounding sub images for
+    // each sub image row
+    public List<List<Integer>> sliceVertical(List<Mat> rows, double whiteRGB) {
+        List<List<Integer>> colsPos = new ArrayList<>();
+        for (int i = 0; i < rows.size(); i++) {
+            colsPos.add(new ArrayList<>());
+            Mat image = rows.get(i);
+            Boolean onWhiteCol = (image.get(0, 0)[0] == whiteRGB);
+            for (int col = 0; col < image.cols(); col++) {
+                Boolean allWhiteCol = true;
+                for (int row = 0; row < image.rows(); row++) {
+                    if (image.get(row, col)[0] != whiteRGB) {
+                        allWhiteCol = false;
+                        break;
+                    }
+                }
+                if (!onWhiteCol && allWhiteCol) {
+                    onWhiteCol = true;
+                    colsPos.get(i).add(col);
+                } else if (onWhiteCol && !allWhiteCol) {
+                    onWhiteCol = false;
+                    colsPos.get(i).add(col);
+                }
+            }
+        }
+        return colsPos;
     }
 
     // REQUIRES: width > 0 && height > 0
-    // image must be a polarized and slice grayscale pixel matrix
+    // MODIFIES: this, image
     // EFFECTS: returns resized image that matches given width and height
     public Mat resizeImage(Mat image, int width, int height) {
-        return null; // stub
+        Imgproc.resize(image, image, new Size(width, height));
+        return image;
     }
 
     // REQUIRES: image must have the same dimensions as each template in templates,
@@ -141,7 +266,19 @@ public class ImageConversion {
     // assigns a similarity score out of 1 for the comparison; returns English
     // character with greatest similarity score
     public String compareWithTemplates(Mat image, List<CharacterTemplate> templates) {
-        return "a"; // stub
+        double maxScore = 0.0;
+        String maxScoreChar = null;
+        image.convertTo(image, CvType.CV_8U);
+        for (CharacterTemplate template : templates) {
+            Mat result = Mat.eye(1, 1, CvType.CV_8U);
+            Imgproc.matchTemplate(image, template.getImageMat(), result, Imgproc.TM_CCOEFF_NORMED);
+            double score = result.get(0, 0)[0];
+            if (score > maxScore) {
+                maxScore = score;
+                maxScoreChar = template.getLabel();
+            }
+        }
+        return maxScoreChar;
     }
 
 }
