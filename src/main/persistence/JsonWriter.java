1f2bf4b840ba5236a24ce66b1bdf338397d715f7
Ryan Xie <yxie84@students.cs.ubc.ca>
2025-02-24 22:51:24 -0800

    Added implementation for JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,6 +1,10 @@
 package persistence;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+import org.json.JSONObject;
 
 import model.ConversionHistory;
 
@@ -8,10 +12,13 @@ import model.ConversionHistory;
 // Adapted from the CPSC 210 JsonSerializationDemo:
 // https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
 public class JsonWriter {
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
 
     // EFFECTS: constructs writer to write to dest(ination) file
     public JsonWriter(String dest) {
-        // stub
+        this.destination = dest;
     }
 
     // MODIFIES: this
@@ -19,25 +26,26 @@ public class JsonWriter {
     // throws FileNotFoundException if destination file
     // cannot be opened for writing
     public void open() throws FileNotFoundException {
-        // stub
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
     // EFFECTS: writes JSON representation of conversion history to file
     public void write(ConversionHistory history) {
-        // stub
+        JSONObject json = history.toJson();
+        saveToFile(json.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
-        // stub
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes string to file
     private void saveToFile(String json) {
-        // stub
+        writer.print(json);
     }
-    
+
 }
