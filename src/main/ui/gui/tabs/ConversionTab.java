d7b89829eb47bfc20a0cd3b1fe205c9437096179
Ryan Xie <yxie84@students.cs.ubc.ca>
2025-03-20 22:03:26 -0700

    Added specification for ConversionTab

---
+++ src/main/ui/gui/tabs/ConversionTab.java
@@ -0,0 +1,49 @@
+package ui.gui.tabs;
+
+import javax.swing.JPanel;
+
+import ui.ImageToTextGUI;
+
+// Conversion tab of Image to Text application GUI
+// Adapted from https://github.students.cs.ubc.ca/CPSC210/LongFormProblemStarters/tree/main/SmartHome
+public class ConversionTab extends Tab {
+
+    // REQUIRES: controller holds this tab
+    // EFFECTS: constructs a conversion tab for image conversion functionality
+    // with list of selectable images, selected image information, and buttons
+    public ConversionTab(ImageToTextGUI controller) {
+        super(controller);
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates area that displays list of selectable images
+    private void placeImageList() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates area that renders selected image
+    private void placeImageRender() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates area that displays selected image's information
+    private void placeImageInfo() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates Convert button
+    private void placeConvertButton() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates area that displays extracted text
+    private void placeExtractedTextArea() {
+        // stub
+    }
+
+}
