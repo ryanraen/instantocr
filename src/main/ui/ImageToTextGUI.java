5a665ac10bffe117f0592a0b88eaf738bbf5dc9f
Ryan Xie <yxie84@students.cs.ubc.ca>
2025-03-20 21:21:25 -0700

    Added specification for ImageToTextGUI

--- src/main/ui/ImageToTextGUI.java
+++ src/main/ui/ImageToTextGUI.java
@@ -10,15 +10,24 @@ import ui.gui.tabs.HistoryTab;
 // GUI interface for Image to Text conversion application
 // Adapted from https://github.students.cs.ubc.ca/CPSC210/LongFormProblemStarters/tree/main/SmartHome
 public class ImageToTextGUI extends JFrame {
+
+    public static void main(String args[]) {
+        // stub
+    }
     
     // MODIFIES: this
     // EFFECTS: constructs ImageToTextGUI, loads selectable images, displays navigation tabs and buttons
-    public ImageToTextGUI() {
+    private ImageToTextGUI() {
         // stub
     }
 
+    // EFFECTS: returns the ImageToTextGUI object controlled by this UI
+    public ImageToTextGUI getGUI() {
+        return null; // stub
+    }
+
     // MODIFIES: this
-    // EFFECTS: loads image files found in data/images
+    // EFFECTS: loads image file paths found in data/images
     private void loadImages() {
         // stub
     }
