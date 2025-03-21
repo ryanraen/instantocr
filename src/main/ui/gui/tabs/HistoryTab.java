7270606665d281f9cbb31321b6a82797b6b5078d
Ryan Xie <yxie84@students.cs.ubc.ca>
2025-03-20 22:03:35 -0700

    Added specification for HistoryTab

---
+++ src/main/ui/gui/tabs/HistoryTab.java
@@ -0,0 +1,42 @@
+package ui.gui.tabs;
+
+import javax.swing.JPanel;
+
+import ui.ImageToTextGUI;
+
+// History tab of Image to Text application GUI
+// Adapted from https://github.students.cs.ubc.ca/CPSC210/LongFormProblemStarters/tree/main/SmartHome
+public class HistoryTab extends Tab {
+
+    // REQUIRES: controller holds this tab
+    // EFFECTS: creates history tab with historical conversions list and buttons
+    public HistoryTab(ImageToTextGUI controller) {
+        super(controller);
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates area that displays list of past conversions
+    private void placeHistoryList() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates area that renders selected conversion image
+    private void placeConversionRender() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates area that displays selected conversion information
+    private void placeConversionInfo() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates Delete and Clear buttons
+    private void placeRemovalButtons() {
+        // stub
+    }
+
+}
