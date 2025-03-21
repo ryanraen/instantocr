fb4fe825f7745b9d689f8ff6df710dced005c0ac
Ryan Xie <yxie84@students.cs.ubc.ca>
2025-03-21 01:43:00 -0700

    Added implementation for Tab

--- src/main/ui/gui/tabs/Tab.java
+++ src/main/ui/gui/tabs/Tab.java
@@ -1,5 +1,7 @@
 package ui.gui.tabs;
 
+import java.awt.FlowLayout;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -8,20 +10,17 @@ import ui.ImageToTextGUI;
 // A tab in the Image to Text application GUI
 // Adapted from https://github.students.cs.ubc.ca/CPSC210/LongFormProblemStarters/tree/main/SmartHome
 public abstract class Tab extends JPanel {
+
+    private final ImageToTextGUI controller;
     
     // REQUIRES: ImageToTextGUI controller that holds this tab
     public Tab(ImageToTextGUI controller) {
-        // stub
-    }
-
-    // EFFECTS: creates and returns row with button included
-    public JPanel formatButtonRow(JButton b) {
-        return null; // stub
+        this.controller = controller;
     }
 
     // EFFECTS: returns the ImageToTextGUI controller for this tab
     public ImageToTextGUI getController() {
-        return null; // stub
+        return controller;
     }
 
 }
