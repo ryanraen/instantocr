65c038d64ade379ceb3a8f1b51647c5deabb6452
Ryan Xie <yxie84@students.cs.ubc.ca>
2025-03-21 17:16:16 -0700

    Added formatButtonRow method in Tab

--- src/main/ui/gui/tabs/Tab.java
+++ src/main/ui/gui/tabs/Tab.java
@@ -1,5 +1,8 @@
 package ui.gui.tabs;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import ui.ImageToTextGUI;
@@ -13,6 +16,15 @@ public abstract class Tab extends JPanel {
     // REQUIRES: ImageToTextGUI controller that holds this tab
     public Tab(ImageToTextGUI controller) {
         this.controller = controller;
+    }
+
+    //EFFECTS: creates and returns row with button included
+    public JPanel formatButtonRow(JButton b) {
+        JPanel p = new JPanel();
+        p.setLayout(new FlowLayout());
+        p.add(b);
+
+        return p;
     }
 
     // EFFECTS: returns the ImageToTextGUI controller for this tab
